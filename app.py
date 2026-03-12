a3527e1449bff99971b036f1cb373b4f497e45fa
marielpagayonan <pagayonanmariel@gmail.com>
2026-03-12 13:41:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
-
 @app.route('/')
 def home():
-    return "Welcome to my Flask API!"
-
+ return "Welcome to my Flask API!"
 @app.route('/student')
 def student():
-    return jsonify({
-        "name": "Charlie Tadlas Goro Jr",
-        "grade": 90,
-        "section": "Zechariah"
-    })
-
-if __name__ == '__main__':
-    app.run(debug=True)
+ return jsonify({
+ "name": "Charlie Tadlas Goro Jr",
+ "grade": 90,
+ "section": "Zechariah"
+ })
